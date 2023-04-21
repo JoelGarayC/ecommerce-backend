908adb2dd524b8f16ff6c260d281e4aca8c4507f
Joel Garay <joelgaray2020@gmail.com>
2023-04-21 00:28:24 -0500

    update

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,8 @@ const pathBase = `/api${version}`
 const developmentAllowedOrigins = [
   `http://localhost:${PORT}`,
   '*',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'http://localhost:5173'
 ]
 const productionAllowedOrigins = [
   'https://ecommerce-backend-rho.vercel.app',
